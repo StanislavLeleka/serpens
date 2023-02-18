8695e9b45c472eb89290bf273f6d86aef15d54e2
Stanislav Leleka <stanislav.leleka@gmail.com>
2023-02-18 13:50:30 +0200

    continue working on matrices

--- matrix/src/matrix.rs
+++ matrix/src/matrix.rs
@@ -1,43 +1,67 @@
+use std::fmt;
+
+enum Dimension {
+    One,
+    Two,
+    Three,
+}
+
+struct Shape(usize, usize);
+
 pub struct Matrix {
-    dim: usize,
+    dim: Dimension,
     size: usize,
+    shape: Shape,
     data: Vec<f64>,
 }
 
 impl Matrix {
     pub fn matrix(elements: Vec<f64>) -> Matrix {
         return Matrix {
-            dim: 1,
+            dim: Dimension::One,
             size: elements.len(),
+            shape: Shape(1, elements.len()),
             data: elements,
         };
     }
 
     pub fn matrix2d(elements: Vec<Vec<f64>>) -> Matrix {
         let size: usize = elements[0].len();
+        let shape: Shape = Shape(elements.len(), size);
         let mut data: Vec<f64> = vec![];
         elements
             .into_iter()
             .for_each(|mut row: Vec<f64>| data.append(&mut row));
 
-        return Matrix { dim: 2, size, data };
+        return Matrix {
+            dim: Dimension::Two,
+            shape,
+            size,
+            data,
+        };
     }
 
     pub fn matrix3d(elements: Vec<Vec<Vec<f64>>>) -> Matrix {
         let size: usize = elements[0][0].len();
+        let shape: Shape = Shape(elements.len(), size);
         let mut data: Vec<f64> = vec![];
         elements.into_iter().for_each(|row: Vec<Vec<f64>>| {
             row.into_iter()
                 .for_each(|mut inner: Vec<f64>| data.append(&mut inner));
         });
-        return Matrix { dim: 3, size, data };
+        return Matrix {
+            dim: Dimension::Three,
+            shape,
+            size,
+            data,
+        };
     }
 
     pub fn print(&self) {
         match self.dim {
-            1 => println!("{:?}", self.data()),
-            2 => self.print2d(),
-            _ => println!("Invalid matrix type!"),
+            Dimension::One => println!("{:?}", self.data()),
+            Dimension::Two => self.print2d(),
+            Dimension::Three => self.print3d(),
         }
     }
 
@@ -47,9 +71,8 @@ impl Matrix {
 
     fn print2d(&self) {
         println!("[");
-        let rows: usize = self.data.len() / self.size;
         let mut skip: usize = 0;
-        for _i in 0..rows {
+        for _ in 0..self.shape.0 {
             let row: Vec<&f64> = self
                 .data
                 .iter()
@@ -57,7 +80,6 @@ impl Matrix {
                 .take(self.size)
                 .into_iter()
                 .collect();
-            print!(" ");
             println!("{:?}", row);
             skip += self.size
         }
@@ -65,10 +87,22 @@ impl Matrix {
     }
 
     fn print3d(&self) {
-        print!("[");
-        let arrays: usize = self.data.len() / self.dim;
-        for a in 0..arrays {
-            let array: Vec<&f64> = self.data.iter().skip(0).take(arrays).into_iter().collect();
+        println!("[");
+        let mut skip: usize = 0;
+        for _ in 0..self.shape.0 {
+            println!("  [");
+            for _ in 0..self.shape.1 {
+                let row: Vec<&f64> = self
+                    .data
+                    .iter()
+                    .skip(skip)
+                    .take(self.size)
+                    .into_iter()
+                    .collect();
+                println!("    {:?}", row);
+                skip += self.size
+            }
+            println!("  ]");
         }
         print!("]");
     }
@@ -83,5 +117,23 @@ fn test_matrix_init() {
     ];
     let matrix: Matrix = Matrix::matrix2d(elements);
 
+    //println!("{:?}", matrix.shape);
     matrix.print();
+
+    let elements: Vec<Vec<Vec<f64>>> = vec![
+        vec![
+            vec![1.2, 2.4, 3.5],
+            vec![4.7, 6.1, 7.2],
+            vec![7.0, 1.0, 7.5],
+        ],
+        vec![
+            vec![1.2, 6.3, 3.5],
+            vec![2.2, 4.1, 4.2],
+            vec![5.4, 0.0, 9.5],
+        ],
+    ];
+    let matrix: Matrix = Matrix::matrix3d(elements);
+    //println!("{:?}", matrix.shape);
+
+    matrix.print3d();
 }
